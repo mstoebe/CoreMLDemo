c6f41f4e8d8b571b5b2f76ba5901cf3eb9450e6e
Markus Stöbe <markus@stoebe.me>
2017-10-08 16:48:00 +0200

    Initial Commit

---
+++ iOS-Squeeze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Squeeze
+//
+//  Created by Markus Stöbe on 08.10.17.
+//  Copyright © 2017 Markus Stöbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
